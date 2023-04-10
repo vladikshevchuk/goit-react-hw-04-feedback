ca69d942ba9e284cb00e1fe42d1392a42c17407f
Vladislav Shevchuk <109815421+vladikshevchuk@users.noreply.github.com>
2023-04-10 16:12:17 +0200

    redesigned classes on hooks

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,34 +1,30 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import './Feedback.css';
 
-class Feedback extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+function Feedback() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const onClickGood = () => {
+    setGood(state => state + 1);
   };
 
-  onClickGood = () => {
-    this.setState(prevState => ({ good: prevState.good + 1 }));
+  const onClickNeutral = () => {
+    setNeutral(state => state + 1);
   };
 
-  onClickNeutral = () => {
-    this.setState(prevState => ({ neutral: prevState.neutral + 1 }));
+  const onClickBad = () => {
+    setBad(state => state + 1);
   };
 
-  onClickBad = () => {
-    this.setState(prevState => ({ bad: prevState.bad + 1 }));
-  };
-
-  countTotalFeedBack = () => {
-    const { good, neutral, bad } = this.state;
+  const countTotalFeedBack = () => {
     let total = good + neutral + bad;
 
     return total;
   };
 
-  countPositiveFeedbackPercentage = () => {
-    const { good, neutral, bad } = this.state;
+  const countPositiveFeedbackPercentage = () => {
     let positiveFeedback = 0;
 
     positiveFeedback = (good / (good + neutral + bad)) * 100;
@@ -40,34 +36,30 @@ class Feedback extends Component {
     return `${positiveFeedback.toFixed(0)} %`;
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-
-    return (
-      <section>
-        <h2>Please leave feedback</h2>
-        <div className="btns">
-          <button type="button" onClick={this.onClickGood}>
-            Good
-          </button>
-          <button type="button" onClick={this.onClickNeutral}>
-            Neutral
-          </button>
-          <button type="button" onClick={this.onClickBad}>
-            Bad
-          </button>
-        </div>
-        <div>
-          <h2>Statistics</h2>
-          <p>Good: {good}</p>
-          <p>Netural: {neutral}</p>
-          <p>Bad: {bad}</p>
-          <p>Total: {this.countTotalFeedBack()}</p>
-          <p>Positive feedback: {this.countPositiveFeedbackPercentage()}</p>
-        </div>
-      </section>
-    );
-  }
-};
+  return (
+    <section>
+      <h2>Please leave feedback</h2>
+      <div className="btns">
+        <button type="button" name='good' onClick={onClickGood}>
+          Good
+        </button>
+        <button type="button" onClick={onClickNeutral}>
+          Neutral
+        </button>
+        <button type="button" onClick={onClickBad}>
+          Bad
+        </button>
+      </div>
+      <div>
+        <h2>Statistics</h2>
+        <p>Good: {good}</p>
+        <p>Netural: {neutral}</p>
+        <p>Bad: {bad}</p>
+        <p>Total: {countTotalFeedBack()}</p>
+        <p>Positive feedback: {countPositiveFeedbackPercentage()}</p>
+      </div>
+    </section>
+  );
+}
 
 export default Feedback;
